7a89f5cb2b9cbca793fb5d021f4107869e906945
kvo24 <152649180+kvo24@users.noreply.github.com>
2024-06-26 12:38:07 -0700

    Delete createRawSchema.sql

--- createRawSchema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists Pub;
-drop table if exists Field;
-
-create table Pub (k text, p text);
-create table Field (k text, i text, p text, v text);
-\copy Pub from '/home/kvo241/cse544-kvo24/hw/hw1/submission/pubFile.txt';
-\copy Field from '/home/kvo241/cse544-kvo24/hw/hw1/submission/fieldFile.txt'; 
